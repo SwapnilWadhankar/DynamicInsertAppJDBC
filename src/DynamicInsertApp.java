e5d5040b72c4f7aef90cff06dbcc35e96b747516
Swapnil Wadhankar <swapnilwadhankar12@gmail.com>
2024-02-14 17:24:23 +0530

    Initial Code before using PreparedStatement Object()

--- src/DynamicInsertApp.java
+++ src/DynamicInsertApp.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class DynamicInsertApp {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DynamicInsertApp {
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = JDBCutil.getJdbcConnection();
+
+            if (connection != null)
+                statement = connection.createStatement();
+
+            if (statement != null)
+                resultSet = statement.executeQuery("select sid,sname,sage,saddress from IPLTeam");
+
+            if (resultSet != null) {
+                System.out.printf("%-2s%14s%12s%15s","SID","SNAME","SAGE","SADDRESS");
+                System.out.println();
+                while (resultSet.next()) {
+                    System.out.printf("%2d%15s%12d%15s", resultSet.getInt(1), resultSet.getString(2),
+                            resultSet.getInt(3), resultSet.getString(4));
+                    System.out.println();
+                }
+            }
+        }catch(IOException ie) {
+            ie.printStackTrace();
+        }catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                JDBCutil.cleanUp(connection, statement, resultSet);
+                System.out.println("closing the resources...");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
--- src/JDBCutil.java
+++ src/JDBCutil.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class JDBCutil {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class JDBCutil {
+
+
+    private JDBCutil() {
+    }
+
+    static {
+        // Step1: loading and register the Driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ce) {
+            ce.printStackTrace();
+        }
+    }
+
+    public static Connection getJdbcConnection() throws SQLException, IOException {
+
+        // Take the data from properties file
+        FileInputStream fis = new FileInputStream("D:\\Projects\\DynamicInputInsertApp\\src\\application\\application.properties");
+        Properties properties = new Properties();
+        properties.load(fis);
+
+        // Step2. Establish the Connection
+        Connection connection = DriverManager.getConnection(properties.getProperty("url"),
+                properties.getProperty("user"), properties.getProperty("password"));
+        System.out.println("connection object created...");
+        return connection;
+    }
+
+    public static void cleanUp(Connection con, Statement statement, ResultSet resultSet) throws SQLException {
+        // Step6. Close the resources
+        if (con != null) {
+            con.close();
+        }
+
+        if (statement != null) {
+            statement.close();
+        }
+        if (resultSet != null) {
+            resultSet.close();
+        }
+    }
 }
